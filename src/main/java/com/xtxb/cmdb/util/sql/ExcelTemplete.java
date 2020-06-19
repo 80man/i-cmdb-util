4a5382139101bf532e8da370ea5d7adcc850adf5
wang645@126.com <19810201>
2020-06-19 11:38:57 +0800

    完成创建Excel模板的类

--- src/main/java/com/xtxb/cmdb/util/sql/ExcelTemplete.java
+++ src/main/java/com/xtxb/cmdb/util/sql/ExcelTemplete.java
@@ -3,7 +3,9 @@ package com.xtxb.cmdb.util.sql;
 import com.xtxb.cmdb.util.Tools;
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.ss.util.CellRangeAddressList;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,13 +21,14 @@ import java.io.FileOutputStream;
 public class ExcelTemplete implements Tools {
     @Override
     public void work(String[] args) {
+        System.out.println("开始生成模板");
         String fileName=null;
         if(args[0].endsWith("/")){
             fileName=args[0]+args[1];
         }else
             fileName=args[0]+ File.separator+args[1];
         writeFile(fileName,createExcel());
-
+        System.out.println("生成模板完成!");
     }
 
     @Override
@@ -54,12 +57,37 @@ public class ExcelTemplete implements Tools {
      * @param sheet
      */
     private void initModel(HSSFSheet sheet){
-        HSSFRow row= sheet.createRow(1);
+        HSSFRow row= sheet.createRow(0);
         HSSFCellStyle style=getStyle(sheet.getWorkbook());
-        HSSFCell cell=row.createCell(1);
+
+        HSSFCell cell=row.createCell(0);
         cell.setCellStyle(style);
-        cell.setCellValue("模型名称");
+        cell.setCellValue("中文名称");
+        sheet.setColumnWidth(0,3000);
+
+        cell=row.createCell(1);
+        cell.setCellStyle(style);
+        cell.setCellValue("英文名称");
         sheet.setColumnWidth(1,3000);
+
+        cell=row.createCell(2);
+        cell.setCellStyle(style);
+        cell.setCellValue("父模型中文名称");
+        sheet.setColumnWidth(2,5000);
+
+        style=getDataStyle(sheet.getWorkbook());
+        row= sheet.createRow(1);
+        cell=row.createCell(0);
+        cell.setCellStyle(style);
+        cell.setCellValue("样例:虚拟机");
+
+        cell=row.createCell(1);
+        cell.setCellStyle(style);
+        cell.setCellValue("C_VM");
+
+        cell=row.createCell(2);
+        cell.setCellStyle(style);
+        cell.setCellValue("服务器");
     }
 
     /**
@@ -67,7 +95,105 @@ public class ExcelTemplete implements Tools {
      * @param sheet
      */
     private void initProperty(HSSFSheet sheet){
+        HSSFRow row= sheet.createRow(0);
+        HSSFCellStyle style=getStyle(sheet.getWorkbook());
 
+        HSSFCell cell=row.createCell(0);
+        cell.setCellStyle(style);
+        cell.setCellValue("中文名称");
+        sheet.setColumnWidth(0,3000);
+
+        cell=row.createCell(1);
+        cell.setCellStyle(style);
+        cell.setCellValue("英文名称");
+        sheet.setColumnWidth(1,3000);
+
+        cell=row.createCell(2);
+        cell.setCellStyle(style);
+        cell.setCellValue("关联模型的中文名称");
+        sheet.setColumnWidth(2,5000);
+
+        cell=row.createCell(3);
+        cell.setCellStyle(style);
+        cell.setCellValue("属性组");
+        sheet.setColumnWidth(3,3000);
+
+        cell=row.createCell(4);
+        cell.setCellStyle(style);
+        cell.setCellValue("属性类型");
+        sheet.setColumnWidth(4,3000);
+
+        cell=row.createCell(5);
+        cell.setCellStyle(style);
+        cell.setCellValue("默认值");
+        sheet.setColumnWidth(5,3000);
+
+        cell=row.createCell(6);
+        cell.setCellStyle(style);
+        cell.setCellValue("校验规则");
+        sheet.setColumnWidth(6,3000);
+
+        cell=row.createCell(7);
+        cell.setCellStyle(style);
+        cell.setCellValue("校验值");
+        sheet.setColumnWidth(7,8000);
+
+        // 加载下拉列表内容
+        DVConstraint constraint = DVConstraint
+                .createExplicitListConstraint(new String[] {"字符串","整型","浮点型","时间型","日期型","日期时间型"});
+        // 设置数据有效性加载在哪个单元格上,四个参数分别是：起始行、终止行、起始列、终止列
+        CellRangeAddressList regions = new CellRangeAddressList(1,
+                100, 4, 4);
+        // 数据有效性对象
+        HSSFDataValidation data_validation_list = new HSSFDataValidation(
+                regions, constraint);
+        sheet.addValidationData(data_validation_list);
+
+        constraint = DVConstraint
+                .createExplicitListConstraint(new String[] {"值域","正则","引用"});
+        // 设置数据有效性加载在哪个单元格上,四个参数分别是：起始行、终止行、起始列、终止列
+        regions = new CellRangeAddressList(1,
+                100, 6, 6);
+        // 数据有效性对象
+        data_validation_list = new HSSFDataValidation(
+                regions, constraint);
+        sheet.addValidationData(data_validation_list);
+
+
+        style=getDataStyle(sheet.getWorkbook());
+        row= sheet.createRow(1);
+        cell=row.createCell(0);
+        cell.setCellStyle(style);
+        cell.setCellValue("样例:CPU位数");
+
+        cell=row.createCell(1);
+        cell.setCellStyle(style);
+        cell.setCellValue("cpubit");
+
+        cell=row.createCell(2);
+        cell.setCellStyle(style);
+        cell.setCellValue("虚拟机");
+
+        cell=row.createCell(3);
+        cell.setCellStyle(style);
+        cell.setCellValue("基本信息");
+
+        cell=row.createCell(4);
+        cell.setCellStyle(style);
+        cell.setCellValue("整型");
+
+        cell=row.createCell(5);
+        cell.setCellStyle(style);
+        cell.setCellValue("64");
+
+
+        cell=row.createCell(6);
+        cell.setCellStyle(style);
+        cell.setCellValue("值域");
+
+        cell=row.createCell(7);
+        cell.setCellStyle(style);
+        cell.setCellValue("32|64，属性值校验规则值，枚举：以 | 分割的多个值； 正则：正则表达式；引用： 关联资源类型的名称");
     }
 
 
@@ -77,15 +203,9 @@ public class ExcelTemplete implements Tools {
      * @return
      */
     private HSSFCellStyle getStyle(HSSFWorkbook workbook) {
-        // 创建单元格样式
         HSSFCellStyle cellStyle = workbook.createCellStyle();
-        // 指定单元格居中对齐
         cellStyle.setAlignment(HorizontalAlignment.CENTER);
-        // 指定单元格垂直居中对齐
         cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-        // 指定当单元格内容显示不下时自动换行
-        cellStyle.setWrapText(true);
-        // 设置单元格字体
         HSSFFont font = workbook.createFont();
         font.setBold(true);
         font.setFontName("宋体");
@@ -93,6 +213,23 @@ public class ExcelTemplete implements Tools {
         cellStyle.setFont(font);
         return cellStyle;
     }
+
+    /**
+     *  创建表格标题行样式
+     * @param workbook
+     * @return
+     */
+    private HSSFCellStyle getDataStyle(HSSFWorkbook workbook) {
+        HSSFCellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setWrapText(true);
+        HSSFFont font = workbook.createFont();
+        font.setFontName("宋体");
+        font.setFontHeight((short) 200);
+        font.setColor(IndexedColors.RED.getIndex());
+        cellStyle.setFont(font);
+        return cellStyle;
+    }
+
     /**
      * Excel信息写入文件
      * @param fileName
