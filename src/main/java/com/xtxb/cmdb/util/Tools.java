ad9d38e6b2445b5414768d77b224c34196eee96a
wang645@126.com <19810201>
2020-06-22 17:39:45 +0800

    添加方法说明

--- src/main/java/com/xtxb/cmdb/util/Tools.java
+++ src/main/java/com/xtxb/cmdb/util/Tools.java
@@ -9,7 +9,16 @@ package com.xtxb.cmdb.util;
  *  用于辅助CMDB管理的工具集的接口
  */
 public interface Tools {
+
+    /**
+     * 执行工具的具体任务
+     * @param args
+     */
     public void work(String[] args);
 
+    /**
+     * 返回所需参说的说明，用于辅助提示工具的参数
+     * @return
+     */
     public String[] getParamList();
 }
